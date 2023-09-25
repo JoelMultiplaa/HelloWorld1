38fed961a19f87623331674579babe1d019f68a5
maztas-tech <mata0001@stud.kea.dk>
2023-09-25 09:41:53 +0200

    Mazlums merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Mazlums first commit and push 2");
         System.out.println("Joels second comit and push");
+
+        Scanner input = new Scanner(System.in);
+
+        int option;
+        do {
+            System.out.println("Choose 1");
+            option = input.nextInt();
+        }while(option != 9);
+
+        System.out.println("Hello there!");
+        System.out.println("General Kenobi!");
     }
 }
